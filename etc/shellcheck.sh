b826e464b7a21c8d211d5c7a7374ee78a172fda2
Morten Terhart <morten.terhart@gmail.com>
2019-10-10 17:04:20 +0200

    [CI] Add etc/ and test/ directories to shellcheck

--- etc/shellcheck.sh
+++ etc/shellcheck.sh
@@ -2,6 +2,6 @@
 
 # Recursively find files with Shebang for sh/bash/ksh
 # and check them using shellcheck
-grep -RE '^#!(/.*/|/usr/bin/env )(sh|bash|ksh)' bin/ lib/ | \
+grep -RE '^#!(/.*/|/usr/bin/env )(sh|bash|ksh)' -- "$@" | \
   sed 's/:.*$//' | \
   xargs shellcheck --format=tty
