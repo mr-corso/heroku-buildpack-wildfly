787d9c7b0d408165c565da5ac7a67d280b2c062f
Morten Terhart <morten.terhart@gmail.com>
2019-10-10 15:05:28 +0200

    Added capturing assertions

---
+++ test/capture_assertions.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+assertCapturedExitCode() {
+    message=""
+
+    if [ $# -eq 2 ]; then
+        message="$1"
+        shift
+    fi
+
+    actualExitCode="$1"
+
+    if [ -z "${RETURN}" ]; then
+        fail "No command was captured before, \$RETURN is null"
+        return 1
+    fi
+
+    # $RETURN is the exit status of the last captured command
+    assertEquals "${message}" "${RETURN}" "${actualExitCode}"
+
+    unset message actualExitCode
+}
+
+assertCapturedStderrContains() {
+    message=""
+
+    if [ $# -eq 2 ]; then
+        message="$1"
+        shift
+    fi
+
+    content="$1"
+
+    if [ ! -f "${STD_ERR}" ]; then
+        fail "ERROR: \$STD_ERR file does not exist"
+        return 1
+    fi
+
+    # Don't confuse this call with the 'assertContains' function provided
+    # by Shunit2. Version 2.1.7 of Shunit2 does not provide the function
+    # yet, but it is provided by the test_utils.sh of the Heroku buildpack
+    # testrunner. This function expects the content to be the first and the
+    # container to be the second argument:
+    #
+    #   assertContains [message] content container
+    #
+    # See https://github.com/heroku/heroku-buildpack-testrunner/blob/master/lib/test_utils.sh
+    # for more information.
+    assertContains "${content}" "$(cat "${STD_ERR}")"
+
+    unset message content
+}
