71d38ef4a2bf4e06a33258a20fe22f9e873005be
Morten Terhart <morten.terhart@gmail.com>
2019-10-11 10:01:57 +0200

    Add WildFly tests

--- test/wildfly_test.sh
+++ test/wildfly_test.sh
@@ -6,6 +6,58 @@ source "${BUILDPACK_HOME}/test/test_helper.sh"
 
 import "wildfly"
 
+createTargetDirectory() {
+    TARGET_DIR="${BUILD_DIR}/target"
+    mkdir -p "${TARGET_DIR}"
+}
+
+createDeployment() {
+    createTargetDirectory
+    echo "This is a WAR file" > "${TARGET_DIR}/deployment.war"
+}
+
+setupJbossHome() {
+    export JBOSS_HOME="${BUILD_DIR}/.jboss/wildfly-16.0.0.Final"
+    mkdir -p "${JBOSS_HOME}"
+    mkdir -p "${JBOSS_HOME}/standalone/deployments"
+}
+
+testDeployWarFiles() {
+    # Don't create target directory
+
+    capture _deploy_war_files "${BUILD_DIR}"
+
+    assertCapturedError 1 "Target directory does not exist"
+
+    createTargetDirectory
+
+    # Don't create WAR files to be deployed
+
+    capture _deploy_war_files "${BUILD_DIR}"
+
+    assertCapturedError 1 "No WAR files found in 'target' directory"
+
+    setupJbossHome
+    createDeployment
+
+    capture _deploy_war_files "${BUILD_DIR}"
+
+    assertCapturedSuccess
+}
+
+testCreateProcessConfiguration() {
+    _create_process_configuration "${BUILD_DIR}"
+
+    local procFile="${BUILD_DIR}/Procfile"
+
+    assertTrue "Procfile does not exist" "[ -f '${procFile}' ]"
+    assertFileContains "web: \${JBOSS_HOME}/bin/standalone.sh -b 0.0.0.0 -Djboss.http.port=\$PORT" "${procFile}"
+
+    _create_process_configuration "${BUILD_DIR}"
+
+    assertTrue "Didn't use existing process type 'web'" "[ '$(wc -l "${procFile}" | awk '{ print $1; }')' -eq 1 ]"
+}
+
 testCreateWildflyProfileScript() {
     _create_wildfly_profile_script "${BUILD_DIR}"
 
@@ -16,4 +68,15 @@ testCreateWildflyProfileScript() {
     assertFileContains "export JBOSS_CLI=" "${profileScript}"
     assertFileContains "export WILDFLY_VERSION=" "${profileScript}"
     assertFileContains "export JAVA_TOOL_OPTIONS=" "${profileScript}"
+}
+
+testCreateWildflyExportScript() {
+    _create_wildfly_export_script "${BUILDPACK_HOME}"
+
+    local exportScript="${BUILDPACK_HOME}/export"
+
+    assertTrue "WildFly export script does not exist" "[ -f '${exportScript}' ]"
+    assertFileContains "export JBOSS_HOME=" "${exportScript}"
+    assertFileContains "export JBOSS_CLI=" "${exportScript}"
+    assertFileContains "export WILDFLY_VERSION=" "${exportScript}"
 }
