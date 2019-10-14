275b66cc9b5526f40c2f090a41842c9133d9ddc4
Morten Terhart <morten.terhart@gmail.com>
2019-10-14 11:08:41 +0200

    Add error test cases for compile script

--- test/compile_test.sh
+++ test/compile_test.sh
@@ -6,6 +6,8 @@ source "${BUILDPACK_HOME}/test/module_loader.sh"
 
 import "wildfly"
 
+import "assertions/capture_assertions"
+import "assertions/file_assertions"
 import "lib/deployment_helper"
 
 ### --- SETUP HOOKS ---
@@ -30,6 +32,7 @@ setUpOnce() {
 
 useCachedWildfly() {
     cp "${WILDFLY_ZIP}" "${CACHE_DIR}"
+    JBOSS_HOME="${BUILD_DIR}/.jboss/wildfly-${DEFAULT_WILDFLY_VERSION}"
 }
 
 createJavaMock() {
@@ -46,9 +49,22 @@ exec /usr/bin/env java "$@"
 EOF
 }
 
+addConfigVar() {
+    local variable="$1"
+    local value="$2"
+
+    echo "${value}" > "${ENV_DIR}/${variable}"
+}
+
+configureWildflyVersion() {
+    local version="$1"
+
+    echo "wildfly.version=${version}" > "${BUILD_DIR}/system.properties"
+}
+
 ### --- TESTS ---
 
-testCompile() {
+testCompileSuccess() {
     useCachedWildfly
     createDeployment
     createJavaMock
@@ -56,4 +72,69 @@ testCompile() {
     compile
 
     assertCapturedSuccess
+
+    # Check that a log was created
+    assertDirExists "${CACHE_DIR}/logs"
+    assertDirNotEmpty "${CACHE_DIR}/logs"
+
+    # Check that JVM common was installed
+    assertDirExists "/tmp/jvm-common"
+
+    # Check that a JDK was installed
+    assertDirExists "${BUILD_DIR}/.jdk"
+    assertFileExists "${BUILD_DIR}/.jdk/bin/java"
+
+    # Check that WildFly 16 was installed and deployed
+    assertDirExists "${JBOSS_HOME}"
+    assertGlobExpands "${JBOSS_HOME}/standalone/deployments/*.war"
+}
+
+testCompileDebug() {
+    useCachedWildfly
+    createDeployment
+    createJavaMock
+
+    addConfigVar "BUILDPACK_DEBUG" "true"
+
+    compile
+
+    assertCapturedSuccess
+    assertCaptured "DEBUG: buildDir=${BUILD_DIR}"
+    assertCaptured "DEBUG: cacheDir=${CACHE_DIR}"
+}
+
+testCompileWithoutTargetDir() {
+    useCachedWildfly
+    createJavaMock
+
+    # Don't create target/ directory
+
+    compile
+
+    assertCapturedError 1 "Could not deploy WAR files: Target directory does not exist"
+}
+
+testCompileWithoutDeployment() {
+    useCachedWildfly
+    createJavaMock
+
+    # Create target/ directory, but no WAR files
+    # for deployment
+    createTargetDirectory
+
+    compile
+
+    assertCapturedError 1 "No WAR files found in 'target' directory"
+}
+
+testCompileInvalidWildflyVersion() {
+    useCachedWildfly
+    createDeployment
+    createJavaMock
+
+    configureWildflyVersion "undefined"
+
+    compile
+
+    assertCapturedError 1 "Unsupported WildFly version: undefined"
 }
